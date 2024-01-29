0848f3916879230a2dff725c9f1c5fc29fc4ddb3
AlexBabkf <bab_alt@hotmail.com>
2024-01-30 00:12:32 +0100

    tests

--- src/components/Calculator.test.js
+++ src/components/Calculator.test.js
@@ -1,35 +1,31 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import Calculator from "./Calculator";
-import test from "node:test";
 
-describe("Calculator Component", () => {
-  test("calculates delivery fee correctly", () => {
-    render(<Calculator />);
+const onCartValueChangeMock = jest.fn();
+const onDeliveryDistanceChangeMock = jest.fn();
+const onNumberOfItemsChangeMock = jest.fn();
+const onOrderTimeChangeMock = jest.fn();
 
-    const onCartValueChangeMock = jest.fn();
-    const onDeliveryDistanceChangeMock = jest.fn();
-    const onNumberOfItemsChangeMock = jest.fn();
-    const onOrderTimeChangeMock = jest.fn();
+const cartValueCases = [
+  {
+    cartValue: 8.2,
+    deliveryPrice: /Delivery price:\s*3.8\s*€/,
+  },
+  {
+    cartValue: 10,
+    deliveryPrice: /Delivery price:\s*2\s*€/,
+  },
+];
 
-    fireEvent.change(screen.getByLabelText("Cart Value:"), {
-      target: { value: "150" },
-    });
-    fireEvent.change(screen.getByLabelText("Delivery Distance:"), {
-      target: { value: "1200" },
-    });
-    fireEvent.change(screen.getByLabelText("Number of Items:"), {
-      target: { value: "6" },
-    });
-    fireEvent.change(screen.getByLabelText("Order Time:"), {
-      target: { value: "2022-01-01T16:00" },
-    });
-
+test.each(cartValueCases)(
+  "Cart value surcharge is calculated for less than 10 euros",
+  (cartValueCase) => {
     render(
       <Calculator
-        cartValue={150}
-        deliveryDistance={1200}
-        numberOfItems={6}
-        orderTime="2022-01-01T16:00"
+        cartValue={cartValueCase.cartValue}
+        deliveryDistance={0}
+        numberOfItems={0}
+        orderTime="2024-01-29T22:28"
         onCartValueChange={onCartValueChangeMock}
         onDeliveryDistanceChange={onDeliveryDistanceChangeMock}
         onNumberOfItemsChange={onNumberOfItemsChangeMock}
@@ -38,6 +34,140 @@ describe("Calculator Component", () => {
     );
 
     fireEvent.click(screen.getByText("Calculate delivery price"));
-    expect(screen.getByText("Delivery price: 5.5 €")).toBeInTheDocument();
-  });
+
+    const element = document.querySelector("p");
+    expect(element.textContent).toMatch(cartValueCase.deliveryPrice);
+  }
+);
+
+const distanceCases = [
+  {
+    deliveryDistance: 999,
+    deliveryPrice: /Delivery price:\s*2\s*€/,
+  },
+  {
+    deliveryDistance: 1001,
+    deliveryPrice: /Delivery price:\s*3\s*€/,
+  },
+];
+
+test.each(distanceCases)("Delivery distance is calculated", (distanceCase) => {
+  render(
+    <Calculator
+      cartValue={10}
+      deliveryDistance={distanceCase.deliveryDistance}
+      numberOfItems={0}
+      orderTime="2024-01-29T22:28"
+      onCartValueChange={onCartValueChangeMock}
+      onDeliveryDistanceChange={onDeliveryDistanceChangeMock}
+      onNumberOfItemsChange={onNumberOfItemsChangeMock}
+      onOrderTimeChange={onOrderTimeChangeMock}
+    />
+  );
+
+  fireEvent.click(screen.getByText("Calculate delivery price"));
+
+  const element = document.querySelector("p");
+  expect(element.textContent).toMatch(distanceCase.deliveryPrice);
+});
+
+const numberOfItemsCases = [
+  {
+    numberOfItems: 4,
+    deliveryPrice: /Delivery price:\s*2\s*€/,
+  },
+  {
+    numberOfItems: 5,
+    deliveryPrice: /Delivery price:\s*2.5\s*€/,
+  },
+  {
+    numberOfItems: 13,
+    deliveryPrice: /Delivery price:\s*7.7\s*€/,
+  },
+];
+
+test.each(numberOfItemsCases)(
+  "Item number fee is calculated",
+  (numberOfItemsCase) => {
+    render(
+      <Calculator
+        cartValue={10}
+        deliveryDistance={0}
+        numberOfItems={numberOfItemsCase.numberOfItems}
+        orderTime="2024-01-29T22:28"
+        onCartValueChange={onCartValueChangeMock}
+        onDeliveryDistanceChange={onDeliveryDistanceChangeMock}
+        onNumberOfItemsChange={onNumberOfItemsChangeMock}
+        onOrderTimeChange={onOrderTimeChangeMock}
+      />
+    );
+
+    fireEvent.click(screen.getByText("Calculate delivery price"));
+
+    const element = document.querySelector("p");
+    expect(element.textContent).toMatch(numberOfItemsCase.deliveryPrice);
+  }
+);
+
+test("Delivery is free when cart value is 200 and more", () => {
+  render(
+    <Calculator
+      cartValue={200}
+      deliveryDistance={3000}
+      numberOfItems={50}
+      orderTime="2024-01-29T22:28"
+      onCartValueChange={onCartValueChangeMock}
+      onDeliveryDistanceChange={onDeliveryDistanceChangeMock}
+      onNumberOfItemsChange={onNumberOfItemsChangeMock}
+      onOrderTimeChange={onOrderTimeChangeMock}
+    />
+  );
+
+  fireEvent.click(screen.getByText("Calculate delivery price"));
+  const deliveryPrice = /Delivery price:\s*0\s*€/;
+
+  const element = document.querySelector("p");
+  expect(element.textContent).toMatch(deliveryPrice);
+});
+
+test("Rush hour is calculated", () => {
+  render(
+    <Calculator
+      cartValue={10}
+      deliveryDistance={0}
+      numberOfItems={0}
+      orderTime="2024-02-02T17:28"
+      onCartValueChange={onCartValueChangeMock}
+      onDeliveryDistanceChange={onDeliveryDistanceChangeMock}
+      onNumberOfItemsChange={onNumberOfItemsChangeMock}
+      onOrderTimeChange={onOrderTimeChangeMock}
+    />
+  );
+
+  fireEvent.click(screen.getByText("Calculate delivery price"));
+  const deliveryPrice = /Delivery price:\s*2.4\s*€/;
+
+  const element = document.querySelector("p");
+  expect(element.textContent).toMatch(deliveryPrice);
+});
+
+test("Fee does not exceed 15 euros", () => {
+  render(
+    <Calculator
+      cartValue={100}
+      deliveryDistance={5000}
+      numberOfItems={250}
+      orderTime="2024-02-02T17:28"
+      onCartValueChange={onCartValueChangeMock}
+      onDeliveryDistanceChange={onDeliveryDistanceChangeMock}
+      onNumberOfItemsChange={onNumberOfItemsChangeMock}
+      onOrderTimeChange={onOrderTimeChangeMock}
+    />
+  );
+
+  fireEvent.click(screen.getByText("Calculate delivery price"));
+  const deliveryPrice = /Delivery price:\s*15\s*€/;
+
+  const element = document.querySelector("p");
+  expect(element.textContent).toMatch(deliveryPrice);
 });
